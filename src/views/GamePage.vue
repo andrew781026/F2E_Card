f4127faed359973517942f9c3dd4098f8e826728
王澍(Andrew) <andrewwang1@micb2b.com>
2020-03-31 17:15:55 +0800

    add footer for main page

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -4,23 +4,55 @@
             <div class="logo">接龍遊戲</div>
             <div class="timer">
                 <div class="title">TIME:</div>
-                <div class="value">10:21</div>
+                <div class="value">{{timeStr}}</div>
                 <div class="title">MOVE:</div>
                 <div class="value">00</div>
             </div>
+        </div>
+        <div class="footer">
+            <img src="../assets/slice_img/icons/icons_stop.svg" alt="stop">
+            <img src="../assets/slice_img/icons/icons_pause.svg" alt="pause">
+            <div class="three-card">
+                <img src="../assets/slice_img/ui/img_threecards.svg" height="70" alt="pause">
+                <div class="button">UNDO</div>
+            </div>
+            <img src="../assets/slice_img/icons/icons_undo.svg" alt="undo">
+            <img src="../assets/slice_img/icons/icons_tips.svg" alt="tips">
         </div>
     </div>
 </template>
 
 <script>
+    import moment from 'moment';
+
     export default {
         name: "GamePage",
         props: {
             counter: Number
         },
+        mounted() {
+
+            this.timer = setInterval(() => this.time++, 1000);
+        },
+        beforeDestroy() {
+
+            clearInterval(this.timer);
+        },
         methods: {
             changePage() {
                 this.$emit('changePage', 'main');
+            }
+        },
+        data() {
+
+            return {
+                time: 0,
+            }
+        },
+        computed: {
+            timeStr() {
+
+                return moment.utc(this.time * 1000).format('mm:ss');
             }
         }
     }
@@ -62,7 +94,37 @@
     }
 
     .footer {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-image: linear-gradient(135deg, rgb(93, 127, 217) 0%, rgb(137, 87, 237) 100%);
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 600px;
+        height: 80px;
+        border-radius: 40px 40px 0 0;
+    }
 
+    .three-card {
+        transform: translateY(-30px);
+        height: 70px;
+    }
+
+    .button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 16px;
+        font-weight: 700;
+        color: white;
+        width: 100px;
+        height: 30px;
+        background-color: #f36b4b;
+        border-radius: 30%/100%;
+        cursor: pointer;
+        transform: translate(5px, -23px);
     }
 
     .button:hover {
